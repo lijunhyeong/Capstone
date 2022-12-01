17dde249fd21b92df56dd2aed4834dc08893423d
junhyungss <daba44@naver.com>
2022-12-01 18:01:47 +0900

    feat(MapActivity): 신고 접수 페이지 이동

--- app/src/main/java/com/atob/capstone/MapActivity.kt
+++ app/src/main/java/com/atob/capstone/MapActivity.kt
@@ -1,11 +1,22 @@
 package com.atob.capstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        intentReportReceived()
+    }
+
+    private fun intentReportReceived(){
+        findViewById<Button>(R.id.reportButton).setOnClickListener {
+            val intent = Intent(this, ReportReceivedActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/atob/capstone/ReportReceivedActivity.kt
@@ -0,0 +1,11 @@
+package com.atob.capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportReceivedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_received)
+    }
+}
