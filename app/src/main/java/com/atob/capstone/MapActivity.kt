82cf5dfc37b4b299097aac00f65c165572092497
junhyungss <daba44@naver.com>
2022-12-01 18:29:00 +0900

    feat(MapActivity): 현재 위치 받아오기 구현 중 - Error Check 하기

--- app/src/main/java/com/atob/capstone/MapActivity.kt
+++ app/src/main/java/com/atob/capstone/MapActivity.kt
@@ -1,17 +1,52 @@
 package com.atob.capstone
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
-class MapActivity : AppCompatActivity() {
+/* 내 위치: https://blog.naver.com/PostView.naver?blogId=glgkwls1&logNo=222469981046&parentCategoryNo=&categoryNo=13&viewDate=&isShowPopularPosts=true&from=search  */
+class MapActivity : AppCompatActivity(), OnMapReadyCallback  {
+
+    private lateinit var mapView: MapView
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
+    private lateinit var naverMap: NaverMap
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        //권한 확인
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
 
+        initSetting()
         intentReportReceived()
+        myPositionButton()
     }
+
+    private fun initSetting(){
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
+    }
+
+    override fun onMapReady(@NonNull naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
+
+
 
     private fun intentReportReceived(){
         findViewById<Button>(R.id.reportButton).setOnClickListener {
@@ -19,4 +54,48 @@ class MapActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun myPositionButton(){
+        findViewById<Button>(R.id.myPosition).setOnClickListener {
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+
 }
